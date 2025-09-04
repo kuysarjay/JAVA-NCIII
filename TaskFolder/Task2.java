d4439c8d1cd0b126e69d28447e488bec3271d66f
Nelson Sto. Domingo <arjaystodomingo07@gmail.com>
2025-09-04 09:11:56 +0000

    Done

---
+++ TaskFolder/Task2.java
@@ -0,0 +1,15 @@
+package TaskFolder;
+public class Task2 {
+    public static void main(String[] args) {
+        byte b = 3;
+        short s = 110;
+        int i = 110;
+        float f = 2.0f;
+        boolean bool = true;
+        char c = 'o';
+
+        String output = "H" + b + i + "w" + c + "rld" + f + " " + bool;
+        System.out.println(output);
+    }
+   
+}
